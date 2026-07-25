bf12c35a8348e603e3b1132f2194efc2a4a63ed8
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-07-25 15:45:02 -0400

    chore: bump to 0.1.0-alpha.9

--- src/main/kotlin/net/sircesarium/qtz/datagen/DataGatherers.kt
+++ src/main/kotlin/net/sircesarium/qtz/datagen/DataGatherers.kt
@@ -1,14 +1,16 @@
 package net.sircesarium.qtz.datagen
 
+import net.minecraft.data.PackOutput
 import net.neoforged.neoforge.data.event.GatherDataEvent
 import net.sircesarium.qtz.api.datagen.DatagenRegistry
 
 object DataGatherers {
     fun gatherData(event: GatherDataEvent) {
+        val unscopedOutput = PackOutput(event.generator.packOutput.outputFolder.parent)
         val modIds = DatagenRegistry.itemModels.map { it.first }.toSet() +
                 DatagenRegistry.handheldModels.map { it.first }.toSet()
         for (modId in modIds) {
-            val provider = QuartzItemModelProvider(event.generator.packOutput, modId, event.existingFileHelper)
+            val provider = QuartzItemModelProvider(unscopedOutput, modId, event.existingFileHelper)
             event.generator.addProvider(true, provider)
         }
     }
