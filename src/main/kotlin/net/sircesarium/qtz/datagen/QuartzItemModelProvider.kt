342f0cc0f0a7d101503ad76e4ab5b7d1aa9054a6
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-07-26 21:06:23 -0400

    fix: remove private val modid from QuartzItemModelProvider to avoid hiding parent field warning

--- src/main/kotlin/net/sircesarium/qtz/datagen/QuartzItemModelProvider.kt
+++ src/main/kotlin/net/sircesarium/qtz/datagen/QuartzItemModelProvider.kt
@@ -6,16 +6,19 @@ import net.neoforged.neoforge.client.model.generators.ItemModelProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import net.sircesarium.qtz.api.datagen.DatagenRegistry
 
-class QuartzItemModelProvider(output: PackOutput, private val modid: String, efh: ExistingFileHelper) :
+class QuartzItemModelProvider(output: PackOutput, modid: String, efh: ExistingFileHelper) :
     ItemModelProvider(output, modid, efh) {
 
     override fun registerModels() {
         for ((entryModId, itemName) in DatagenRegistry.itemModels) {
             if (entryModId != modid) continue
+
             basicItem(ResourceLocation.fromNamespaceAndPath(entryModId, itemName))
         }
+
         for ((entryModId, itemName) in DatagenRegistry.handheldModels) {
             if (entryModId != modid) continue
+
             handheldItem(ResourceLocation.fromNamespaceAndPath(entryModId, itemName))
         }
     }
