6c88703bd3b494ba5d703ddf6e36ddb09ceb47bd
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-07-28 18:38:02 -0400

    feat(api): add BlockBuilder.itemFactory and createItem helper

--- src/main/kotlin/net/sircesarium/qtz/api/block/BlockBuilder.kt
+++ src/main/kotlin/net/sircesarium/qtz/api/block/BlockBuilder.kt
@@ -1,5 +1,6 @@
 package net.sircesarium.qtz.api.block
 
+import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockBehaviour
@@ -8,6 +9,7 @@ class BlockBuilder<B : Block> {
     @PublishedApi internal var itemCustomizer: (Item.Properties.() -> Unit)? = null
     @PublishedApi internal var blockCustomizer: (BlockBehaviour.Properties.() -> Unit)? = null
     @PublishedApi internal var blockFactory: ((BlockBehaviour.Properties) -> B)? = null
+    @PublishedApi internal var itemFactory: ((Block, Item.Properties) -> Item)? = null
 
     fun configureItem(action: Item.Properties.() -> Unit) {
         this.itemCustomizer = action
@@ -20,4 +22,17 @@ class BlockBuilder<B : Block> {
     fun customBlock(factory: (BlockBehaviour.Properties) -> B) {
         this.blockFactory = factory
     }
+
+    fun customItem(factory: (Block, Item.Properties) -> Item) {
+        this.itemFactory = factory
+    }
+}
+
+internal fun <B : Block> createItem(
+    builder: BlockBuilder<B>,
+    blockHolder: net.neoforged.neoforge.registries.DeferredBlock<B>,
+    properties: Item.Properties,
+): Item {
+    return builder.itemFactory?.invoke(blockHolder.get(), properties)
+        ?: BlockItem(blockHolder.get(), properties)
 }
